b87d0f2fae8aacd772294401d272fee345e20d0e
Levon <levon.dev@gmail.com>
2020-12-08 20:14:24 +0300

    Day 5 part two completed

--- 5/solution.py
+++ 5/solution.py
@@ -10,20 +10,31 @@ def binary_search(left, right, lower_char, higher_char, chars):
     assert(left == right)
     return left
 
-def decode_seat(value):
+def decode_seats(value):
     return (binary_search(0, 127, 'F', 'B', value[:7]), binary_search(0, 7, 'L', 'R', value[7:]))
 
 def seat_id(row, column):
     return row * 8 + column
 
 if __name__ == "__main__":
-    row, column = decode_seat('BFFFBBFRRR')
+    row, column = decode_seats('BFFFBBFRRR')
     assert(row == 70 and column == 7 and seat_id(row, column) == 567)
-    row, column = decode_seat('FFFBBBFRRR')
+    row, column = decode_seats('FFFBBBFRRR')
     assert(row == 14 and column == 7 and seat_id(row, column) == 119)
-    row, column = decode_seat('BBFFBBFRLL')
+    row, column = decode_seats('BBFFBBFRLL')
     assert(row == 102 and column == 4 and seat_id(row, column) == 820)
 
     with open('input') as source:
-        print(max(seat_id(*decode_seat(line.strip())) for line in source))
-
+        all_seats = set()
+        for x in range(128):
+            for y in range(8):
+                all_seats.add( (x, y) )
+        in_list = set(decode_seats(line.strip()) for line in source)
+        possible = all_seats - in_list
+        # filter out isle seats
+        possible = filter(lambda pair: pair[1] != 0 and pair[1] != 7, possible)
+        # left and right neighbours required
+        for row, column in possible:
+            if (row, column - 1) in in_list and (row, column + 1) in in_list:
+                print(seat_id(row, column))
+                break
