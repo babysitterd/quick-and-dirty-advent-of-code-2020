553df7ae53e75ebada0992126957d6d4275867b6
levon <levon.dev@gmail.com>
2020-12-11 10:13:45 +0300

    Day 7 part two completed

--- 7/solution.py
+++ 7/solution.py
@@ -2,27 +2,53 @@
 
 import re
 
-def check_branch(bag, result, tree):
+def bag_contained_in(bag, result, tree):
     if bag not in tree:
         return
 
     for i in tree[bag]:
         result.add(i)
-        check_branch(i, result, tree)
+        bag_contained_in(i, result, tree)
+
+def bag_contains(bag, tree):
+    if bag not in tree:
+        return 0
+
+    result = 0
+    for bag, count in tree[bag]:
+        result += count * (bag_contains(bag, tree) + 1)
+    return result
 
 if __name__ == "__main__":
     with open('input') as source:
         contained_in = { }
-        pattern = re.compile(r'(\w+) (\w+) bag')
+        contains = { }
+        pattern = re.compile(r'(\d*) ?(\w+) (\w+) bag')
         for line in source:
             match = pattern.findall(line)
-            if not match or match[1] == ('no', 'other'):
+            if not match or match[1][1:] == ('no', 'other'):
                 continue
+            large = ' '.join(match[0][1:])
+            contains[large] = set()
             for bag in match[1:]:
-                if bag in contained_in:
-                    contained_in[bag].add(match[0])
+                small = ' '.join(bag[1:])
+                contains[large].add((small, int(bag[0]) if bag[0] else 1))
+                if small in contained_in:
+                    contained_in[small].add(large)
                 else:
-                    contained_in[bag] = { match[0] }
+                    contained_in[small] = { large }
+        # part one
         result = set()
-        check_branch(('shiny', 'gold'), result, contained_in)
+        bag_contained_in('shiny gold', result, contained_in)
         print(len(result))
+        # part two
+        print(bag_contains('shiny gold', contains))
+
+#        tests for sample dataset
+#        bag_contained_in('shiny gold', result, contained_in)
+#        assert(len(result) == 4)
+#        assert(bag_contains('faded blue', contains) == 0)
+#        assert(bag_contains('dotted black', contains) == 0)
+#        assert(bag_contains('vibrant plum', contains) == 11)
+#        assert(bag_contains('dark olive', contains) == 7)
+#        assert(bag_contains('shiny gold', contains) == 32)
